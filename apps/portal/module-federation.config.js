995a035914a742ed3e661e6875a52097ec05f00f
sapphinebz <sapphinebz@gmail.com>
2023-08-15 20:23:20 +0700

    added config custom

--- apps/portal/module-federation.config.js
+++ apps/portal/module-federation.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   name: 'portal',
   remotes: [],
+  // shared: (name, config) => {
+  // We want lodash to be tree shaken, and bundled into each host/remote separately.
+  //     '@angular/core': { singleton: true },
+  //     '@angular/common': { singleton: true },
+  //     '@angular/router': { singleton: true },
+  // if (
+  //   ['@angular/core', '@angular/common', '@angular/router'].includes(name)
+  // ) {
+  // config.requiredVersion = false;
+  // config.strictVersion = false;
+  // config.singleton = false;
+  // config.singleton = true;
+  // config.strictVersion = true;
+  // config.requiredVersion = 'auto';
+  // console.log(config);
+  // }
+  // },
 };
--- apps/portal/webpack.config.js
+++ apps/portal/webpack.config.js
@@ -1,3 +1,29 @@
 const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
-module.exports = withModuleFederation(config);
+const { ModuleFederationPlugin } = require('webpack').container;
+module.exports = async (wco) => {
+  const wmf = await withModuleFederation(config);
+  return wmf({
+    ...wco,
+    plugins: [
+      ...(wco.plugins ?? []),
+      //   new ModuleFederationPlugin({
+      //     shared: {
+      //       '@angular/core': { singleton: true },
+      //       '@angular/common': { singleton: true },
+      //       '@angular/router': { singleton: true },
+      //     },
+      //   }),
+      //   new LicenseWebpackPlugin({
+      //     stats: {
+      //       warnings: false,
+      //       errors: false,
+      //     },
+      //     perChunkOutput: false,
+      //     outputFilename: '3rdpartylicenses.txt',
+      //     skipChildCompilers: true,
+      //     modulesDirectories: [path.resolve(__dirname, '../../node_modules')],
+      //   }),
+    ],
+  });
+};
